99fa07d5994da9c43111a95615ba92aa8c8eaabd
Sam Carlton <sam@sam.lc>
2020-02-09 00:21:22 -0600

    Style header typography

--- src/components/header.js
+++ src/components/header.js
@@ -13,6 +13,8 @@ const Header = ({ siteTitle }) => (
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
+        textAlign: 'center',
+        textTransform: 'uppercase'
       }}
     >
       <h1 style={{ margin: 0 }}>
@@ -21,6 +23,8 @@ const Header = ({ siteTitle }) => (
           style={{
             color: `white`,
             textDecoration: `none`,
+            fontWeight: '900',
+            fontStretch: '75%',
           }}
         >
           {siteTitle}
