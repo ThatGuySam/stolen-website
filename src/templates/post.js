eecaf94bf704db1c72096bd7b86ee2b4857da949
Sam Carlton <sam@sam.lc>
2020-02-08 23:07:34 -0600

    Use srcset image for posts

--- src/templates/post.js
+++ src/templates/post.js
@@ -9,7 +9,8 @@ class Post extends Component {
   render() {
     const post = this.props.data.instaNode
 
-    const image = post.localFile.childImageSharp.fixed
+    // const image = post.localFile.childImageSharp.fixed
+    const image = post.localFile.childImageSharp
 
     console.log('post', post)
 
@@ -17,9 +18,23 @@ class Post extends Component {
         <Layout>
             <SEO title="Home" />
             <div>
-                <img
-                    src={image.src}
-                />
+                <picture>
+                    <source
+                        type="image/jpg"
+                        sizes={image.sizes.sizes}
+                        srcSet={image.sizes.srcSet}
+                    />
+                    <source
+                        type="image/webp"
+                        sizes={image.sizes.sizes}
+                        srcSet={image.sizes.srcSetWebp}
+                    />
+                    <img
+                        src={image.sizes.src}
+                        style={{width: '100%'}}
+                        alt='Instagram Meme'
+                    />
+                </picture>
                 <h1>{post.likes}</h1>
                 <div>{post.caption}</div>
             </div>
@@ -42,11 +57,12 @@ export const postQuery = graphql`
         caption
         localFile {
             childImageSharp {
-                fixed {
-                    width
-                    height
-                    src
+                sizes {
                     srcSet
+                    src
+                    srcSetWebp
+                    srcWebp
+                    sizes
                 }
             }
         }
